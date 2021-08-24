fda9712be72b3c7b2e7b46e4ba5a41d975ea6f32
Claudia <clau_givan@yahoo.com>
2021-08-24 21:07:25 +0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import http from 'http';
+import path from 'path';
+import serveStatic from 'serve-static';
+import render from './server-render';
+// @ts-expect-error handle by rollup-plugin-string
+import template from './index.html';
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const serve = serveStatic(path.resolve(__dirname, '../client'));
+
+http.createServer(async(req, res) => {
+  const url = req.url as string;
+  console.log(url);
+
+  if (url.startsWith('/_assets/')) {
+    serve(req, res, () => {
+      res.statusCode = 404;
+      res.end('Not Found');
+    });
+  } else {
+    const { error, status, headers, body } = await render(
+      {
+        url,
+        template,
+
+        ctx: {
+          cookies: req.headers.cookie
+            ? Object.fromEntries(
+              new URLSearchParams(req.headers.cookie.replace(/;\s*/g, '&')).entries()
+            )
+            : {},
+
+          headers: req.headers
+        }
+      }
+    );
+
+    if (error) {
+      console.error(error);
+    }
+
+    res.writeHead(status, headers);
+    res.end(body);
+  }
+}).listen(PORT);
